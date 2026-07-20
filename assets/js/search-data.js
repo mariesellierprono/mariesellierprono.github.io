17f795371a0a88364179ace9e1e9742807c2341a
mariesellierprono <92634989+mariesellierprono@users.noreply.github.com>
2026-07-20 08:03:52 +0000

    Deploying to gh-pages from @ mariesellierprono/mariesellierprono.github.io@433dd284f471916d0ae6621a691e2823d74143cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,6 +49,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/marie-sellier-prono-b315a417a", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=9VrfY64AAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
